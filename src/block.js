a8b1c8faa9bc50fb8854e44d21c665bb41aaa0a1
Jolanta Jasiulinyte <klpdjolanta@gmail.com>
2018-01-20 11:07:11 +0000

    Classes Block

--- src/block.js
+++ src/block.js
@@ -21,5 +21,11 @@ class Block {
     ).toString();
   }
 
-  mineBlock(difficulty) 
+  mineBlock(difficulty) {
+    while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
+      this.nonce++;
+      this.hash = this.calculateHash();
+    }
+    console.log("BLOCK MINED:" +  this.hash);
+  }
 }
