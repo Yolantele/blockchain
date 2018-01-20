2208fe9a8de1ea5a0574957e03fbf868e19801c9
Jolanta Jasiulinyte <klpdjolanta@gmail.com>
2018-01-20 11:07:07 +0000

    Classes Block

---
+++ src/block.js
@@ -0,0 +1,25 @@
+
+const SHA = require("crypto-js/sha256");
+
+class Block {
+  constructor(index, timestamp, data, previousHash = '') {
+    this.index = index;
+    this.previousHash = previousHash;
+    this.timestamp = timestamp;
+    this.data = data;
+    this.hash = this.calculateHash();
+    this.notice = 0;
+  }
+
+  calculateHash() {
+    return SHA(
+      this.index +
+      this.previousHash +
+      this.timestamp +
+      JSON.stringify(this.data) +
+      this.nonce
+    ).toString();
+  }
+
+  mineBlock(difficulty) 
+}
