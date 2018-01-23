d25f9c64d064a6db9e1a8d825ab8d402921294a8
Jolanta Jasiulinyte <klpdjolanta@gmail.com>
2018-01-23 10:34:52 +0000

    Uniform style eslint set up

--- src/block.js
+++ src/block.js
@@ -1,31 +1,6 @@
 
-const SHA = require("crypto-js/sha256");
+const SHA = require('crypto-js/sha256');
 
 class Block {
-  constructor(index, timestamp, data, previousHash = '') {
-    this.index = index;
-    this.previousHash = previousHash;
-    this.timestamp = timestamp;
-    this.data = data;
-    this.hash = this.calculateHash();
-    this.notice = 0;
-  }
 
-  calculateHash() {
-    return SHA(
-      this.index +
-      this.previousHash +
-      this.timestamp +
-      JSON.stringify(this.data) +
-      this.nonce
-    ).toString();
-  }
-
-  mineBlock(difficulty) {
-    while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
-      this.nonce++;
-      this.hash = this.calculateHash();
-    }
-    console.log("BLOCK MINED:" +  this.hash);
-  }
 }
