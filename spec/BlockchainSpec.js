4102098829ef01df26c0341cb772d3d54fc8eb89
Jolanta Jasiulinyte <klpdjolanta@gmail.com>
2018-01-23 17:39:00 +0000

    Gets Latest Block wip

--- spec/BlockchainSpec.js
+++ spec/BlockchainSpec.js
@@ -3,14 +3,33 @@
 describe('Blockchain', () => {
   var Blockchain = require('../lib/Blockchain');
   var blockchain;
+  var blockDummy;
+
+  var counter = 0;
+
+  function BlockDummy () {
+    // counter += 1;
+    // this.counter = counter;
+  };
 
   beforeEach(() => {
     blockchain = new Blockchain();
+    blockDummy = new BlockDummy();
   })
 
   describe('initialize', () => {
     it('is an instance of blockchain', () => {
-      expect(blockchain).toEqual(new Blockchain ())
+      expect(blockchain).toEqual(new Blockchain())
+    });
+  });
+  describe('createGenesisBlock', () => {
+    it('returns new Block', () => {
+      expect(blockchain.createGenesisBlock(BlockDummy)).toEqual(new BlockDummy)
+    });
+  });
+  describe('getLatestBlock', () => {
+    it('returns the last object in the chain ', () => {
+      expect(blockchain.getLatestBlock()).toEqual('last block')
     });
   });
 });
