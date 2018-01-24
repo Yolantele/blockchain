eced8611f11f25d279a5186b8f260d223ada3816
Jolanta Jasiulinyte <klpdjolanta@gmail.com>
2018-01-24 06:57:51 +0000

    Eslint file clean-up

--- spec/BlockchainSpec.js
+++ spec/BlockchainSpec.js
@@ -1,40 +1,38 @@
 //_____BLOCKCHAIN CLASS _____:
 
 describe('Blockchain', () => {
-  var Blockchain = require('../lib/Blockchain');
-  var blockchain;
-  var blockDummy;
+	var Blockchain = require('../lib/Blockchain');
+	var blockchain;
+	var blockDummy;
 
-  var counter = 0;
+	var counter = 0;
 
-  function BlockDummy () {
-    // counter += 1;
-    // this.counter = counter;
-  };
+	function BlockDummy () {
+		// counter += 1;
+		// this.counter = counter;
+	}
 
-  beforeEach(() => {
-    blockchain = new Blockchain();
-    blockDummy = new BlockDummy();
-  })
+	beforeEach(() => {
+		blockchain = new Blockchain();
+		blockDummy = new BlockDummy();
+	});
 
-  describe('initialize', () => {
-    it('is an instance of blockchain', () => {
-      expect(blockchain).toEqual(new Blockchain())
-    });
-  });
-  describe('createGenesisBlock', () => {
-    it('returns new Block', () => {
-      expect(blockchain.createGenesisBlock(BlockDummy)).toEqual(new BlockDummy)
-    });
-  });
-  describe('getLatestBlock', () => {
-    it('returns the last object in the chain ', () => {
-      expect(blockchain.getLatestBlock()).toEqual('last block')
-    });
-  });
+	describe('initialize', () => {
+		it('is an instance of blockchain', () => {
+			expect(blockchain).toEqual(new Blockchain());
+		});
+	});
+	describe('createGenesisBlock', () => {
+		it('returns new Block', () => {
+			expect(blockchain.createGenesisBlock(BlockDummy)).toEqual(new BlockDummy);
+		});
+	});
+	describe('getLatestBlock', () => {
+		it('returns the last object in the chain ', () => {
+			expect(blockchain.getLatestBlock()).toEqual('last block');
+		});
+	});
 });
-
-
 
 
 // _____ TEMPLATES _____
